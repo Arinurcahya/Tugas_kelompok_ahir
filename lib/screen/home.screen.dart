9a5701b705bf65d44c38d60ab54d996c16f6727a
palcomtech <elliciaserliyaw@gmail.com>
2023-06-21 10:59:36 +0700

    home_screen

---
+++ lib/screen/home.screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:tugas_kelompok_ahir/models/user.dart';
+import 'package:tugas_kelompok_ahir/Models/user_respository.dart';
+import 'package:tugas_kelompok_ahir/screen/edit_screen.dart';
+import 'package:tugas_kelompok_ahir/screen/inputkaryawan_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  final UserRepository userRepository;
+
+  const HomeScreen({required this.userRepository});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<User>> _usersFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _usersFuture = widget.userRepository.getUsers();
+  }
+
+  void _goToAddScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => InputKaryawanScreen(
+          userRepository: widget.userRepository,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MENU'),
+      ),
+      body: FutureBuilder<List<User>>(
+        future: _usersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final List<User> employees = snapshot.data ?? [];
+
+          return ListView.builder(
+            itemCount: employees.length,
+            itemBuilder: (context, index) {
+              final User employee = employees[index];
+              return ListTile(
+                title: Text(employee.name),
+                subtitle: Text(employee.email),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditScreen(
+                        user: employee,
+                        userRepository: widget.userRepository,
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: Row(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
+    FloatingActionButton(
+      onPressed: () {
+        // Navigasi ke halaman orang
+      },
+      child: Icon(Icons.person),
+    ),
+    SizedBox(width: 16.0),
+    FloatingActionButton(
+      onPressed: _goToAddScreen,
+      child: Icon(Icons.people),
+    ),
+    SizedBox(width: 16.0),
+    FloatingActionButton(
+      onPressed: () {
+        // Navigasi ke halaman barang
+      },
+      child: Icon(Icons.shopping_cart),
+    ),
+  ],
+),
+    );
+  }
+}

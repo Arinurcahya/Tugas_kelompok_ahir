5fe6f73c19d648ebe7a8f7ed7e9623af8de41e0a
hest <hesta@gmail.com>
2023-07-12 01:00:15 +0700

    profil

--- lib/screen/profil_screen.dart
+++ lib/screen/profil_screen.dart
@@ -6,6 +6,7 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile PT Karet'),
+        backgroundColor: Colors.orange,
       ),
       body: Center(
         child: Column(
@@ -15,33 +16,66 @@ class ProfileScreen extends StatelessWidget {
               radius: 80,
               backgroundImage: AssetImage('assets/images/karet.jpg'),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 24),
             Text(
               'PT Karet',
-              style: TextStyle(fontSize: 24),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
             SizedBox(height: 8),
             Text(
               'Perusahaan Karet Terkemuka',
               style: TextStyle(fontSize: 16),
             ),
+            SizedBox(height: 24),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Alamat:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'Jl. Jend. Sudirman No. 123',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             SizedBox(height: 16),
-            Text(
-              'Alamat:',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Jl. Jend. Sudirman No. 123',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Email:',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'info@ptkaret.com',
-              style: TextStyle(fontSize: 16),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email:',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      'info@ptkaret.com',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
